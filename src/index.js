aec2288d6946f4bc8d2815e3abd6cfe485881de5
Peter Njuguna <Ptahbn@gmail.com>
2022-08-10 15:01:22 +0300

    Add functionality

--- src/index.js
+++ src/index.js
@@ -1,16 +1,70 @@
-// import _ from 'lodash';
+import * as task from './task.js';
+import * as stat from './state.js';
 import './style.css';
 
-const todo = [
-  { description: 'Wash the dishes', isCompleted: false, index: 0 },
-  { description: 'Complete To Do List Project', isCompleted: false, index: 1 },
-];
+let lists = [];
+const listEl = document.querySelector('ul');
 
 const todoList = () => {
-  let todoListContent = '';
-  todo.forEach((item) => {
-    todoListContent += `<li class="task"><input class="task-check" type="checkbox"><span class="list">${item.description}</span><i class='fa fa-ellipsis-v' style="margin-left: auto"></i></li>`;
+  if (window.localStorage.getItem('localTasks')) {
+    const localTasks = window.localStorage.getItem('localTasks');
+    lists = JSON.parse(localTasks);
+  }
+  document.querySelector('.task-box').innerHTML = '';
+  lists.forEach((item) => {
+    const taskElement = document.createElement('li');
+    taskElement.classList.add('task');
+    if (item.isCompleted) { taskElement.classList.add('completed'); }
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.classList.add('task-check');
+    checkbox.addEventListener('click', () => {
+      stat.state(item, lists);
+      todoList();
+    });
+    checkbox.checked = item.isCompleted;
+    taskElement.appendChild(checkbox);
+    const taskText = document.createElement('input');
+    taskText.classList = 'task-text';
+    taskText.value = item.description;
+    taskText.addEventListener('change', () => {
+      if (taskText.value.length > 0) {
+        item.description = taskText.value;
+        stat.saveLocal(lists);
+      }
+    });
+    taskElement.appendChild(taskText);
+    const dragIcon = document.createElement('button');
+    dragIcon.classList = 'far fa-trash-alt deleteBtn';
+    taskElement.appendChild(dragIcon);
+    taskElement.draggable = 'true';
+    document.querySelector('.task-box').appendChild(taskElement);
   });
-  document.querySelector('.task-box').innerHTML = todoListContent;
 };
+
+const removeItem = (e) => {
+  if (!e.target.classList.contains('deleteBtn')) {
+    return;
+  }
+  const btn = e.target;
+  lists.forEach((task) => {
+    if (task.description === btn.parentElement.children[1].value) {
+      lists.splice(lists.indexOf(task), 1);
+    }
+  });
+  btn.closest('li').remove();
+  task.updateIndex(lists);
+  stat.saveLocal(lists);
+};
+
+listEl.addEventListener('click', removeItem);
 todoList();
+document.querySelector('#task-input').addEventListener('submit', (event) => {
+  event.preventDefault();
+  task.add(lists);
+  todoList();
+});
+document.querySelector('.clear-container').addEventListener('click', () => {
+  task.removeDone(lists);
+  todoList();
+});
